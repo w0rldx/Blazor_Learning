045a3a2aa7c29b0ec35fa3a2660279f8f6ff66b4
Michael <michaelgmx1@googlemail.com>
2020-11-25 23:42:38 +0100

    Update note manager web

---
+++ Task Manager/Task Manager/Pages/Notes/TaskModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Task_Manager.Pages.Notes
+{
+    public class TaskModel
+    {
+        public Guid Guid { get; set; }
+        
+        [Required(ErrorMessage = "Titel wird benötigt")]
+        [StringLength(20, ErrorMessage = "Titel ist zu lang")]
+        public string Titel { get; set; }
+        
+        [Required(ErrorMessage = "Inhalt wird benötigt")]
+        public string Inhalt { get; set; }
+        public DateTime Erstellt { get; set; }
+        public bool ReadOnly { get; set; } = true;
+
+        // public TaskModel(string titel, string inhalt, bool readOnly)
+        // {
+        //     Titel = titel;
+        //     Inhalt = inhalt;
+        //     Erstellt = DateTime.Now;
+        //     ReadOnly = readOnly;
+        // }
+    }
+}
